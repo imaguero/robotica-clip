a043c8f2ec93dcfd61c8283b93674960890a9f46
Ignacio Agüero <ignaciomaguero@gmail.com>
2020-09-10 15:08:03 -0300

    se incorporo js

---
+++ JavaScript/juego.js
@@ -0,0 +1,30 @@
+var btnAbrirPopup = document.getElementById("btn-abrir-popup")
+var max = 3;
+var min = 3;
+var numerom = Math.random()*(max-min) + min;
+numerom = parseInt(numerom);
+
+while(true){
+    var usuario = prompt("Bienvenido! Responde a la siguiente pegunta: Cuantas generaciones de Robots existen? Coloca un número para adivinar. Sólo tienes 4 intentos posibles.");
+
+if(usuario == numerom){
+    alert("Has Acertado!");
+    break;
+}
+else if(usuario == 0){
+    break;
+}
+else if(usuario < numerom){
+
+    alert("Ups, tu número debería ser más alto" );
+}
+else if(usuario > numerom){
+
+
+    alert("Ups, tu número debería ser mas bajo");
+}
+
+
+
+
+}
